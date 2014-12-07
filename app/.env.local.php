4cc4f2710fa1d121c850e1b786410641a3264f70
Bruno Barros <brunodanca@gmail.com>
2014-12-07 18:36:28 -0200

    removing .env.local.php as .env.php is the fallback

--- app/.env.local.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if(file_exists('../.env.local.php'))
-{
-	return require '../.env.local.php';
-}
--- app/.env.php
+++ app/.env.php
@@ -1,6 +1,14 @@
 <?php
 
-if(file_exists('../.env.php'))
+if (file_exists('../.env.local.php'))
+{
+	return require '../.env.local.php';
+}
+elseif (file_exists('../.env.php'))
 {
 	return require '../.env.php';
+}
+else
+{
+	throw new Exception("Define a .env file on your root folder.");
 }
