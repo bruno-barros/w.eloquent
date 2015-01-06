f99f4543d4c82cda626dfde4b4319f1e513f869b
Bruno Barros <brunodanca@gmail.com>
2015-01-06 21:18:53 -0200

    added script to prevent theme errors

---
+++ src/mu-plugins/_prevent_theme_error.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * --------------------------------------------------------------------
+ *  Check if active theme match APP_THEME and is available
+ * --------------------------------------------------------------------
+ *
+ *  !! You should remove it on production !!
+ *
+ *  It is just to prevent errors when installing and setting up.
+ */
+
+if (wp_get_theme()->get_template() !== APP_THEME || wp_get_theme()->errors() !== false)
+{
+
+	$themes = array_filter(scandir(SRC_PATH . '/themes/'), function ($dir)
+	{
+		return substr($dir, 0, 1) != '.';
+	});
+
+
+	if (in_array(APP_THEME, $themes))
+	{
+		update_option('template', APP_THEME);
+		update_option('stylesheet', APP_THEME);
+	}
+	else
+	{
+		printf('<h1>%s</h1>', 'Unable to locate [' . APP_THEME . '] theme directory. Fix it on your .env*.php file.');
+		exit;
+	}
+}
