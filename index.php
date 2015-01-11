7223bb1fc901d53d4db44c12c6078aa1c2713398
Bruno Barros <brunodanca@gmail.com>
2015-01-11 18:01:49 -0200

    clean up comments

--- index.php
+++ index.php
@@ -1,29 +1,10 @@
 <?php
-
-/*----------------------------------------------------*/
-// Local environment vars => .env.local.php
-// Production environment vars => .env.production.php
-/*----------------------------------------------------*/
-/*
-return array(
-
-	// if the .env.local.php is present the environment will be set to 'local'
-	// by default, but you can set a personal environment configuration for some
-	// part of the application
-	'APP_ENV'     => 'local',
-
-	'APP_THEME'   => 'base',
-
-	'DB_NAME'     => '',
-	'DB_USER'     => '',
-	'DB_PASSWORD' => '',
-	'DB_HOST'     => 'localhost',
-	'DB_PREFIX'   => 'wp_',
-	'WP_HOME'     => 'http://localhost/weloquent',
-	'WP_SITEURL'  => 'http://localhost/weloquent/cms',
-
-);
-*/
+/**
+ * ----------------------------------------------------
+ * w.eloquent
+ * @link https://github.com/bruno-barros/w.eloquent
+ *-----------------------------------------------------
+ */
 
 /**
  * Tells WordPress to load the WordPress theme and output it.
@@ -32,5 +13,7 @@ return array(
  */
 define('WP_USE_THEMES', true);
 
-/** Loads the WordPress Environment and Template */
+/**
+ * Loads the WordPress Environment and Template
+ */
 require(dirname(__FILE__) . '/cms/wp-blog-header.php');
