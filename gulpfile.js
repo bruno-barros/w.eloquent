9e33bca81978cde7c7a28e2a90615e9e7b88f919
Bruno Barros <brunodanca@gmail.com>
2015-06-24 01:08:27 -0300

    added browserify and browser-sync, moved braind packages to project composer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
-
+var fs = require('fs');
+var pkg = JSON.parse(fs.readFileSync('./package.json'));
 // Include Our Plugins
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
@@ -8,29 +9,30 @@ var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var stripDebug = require('gulp-strip-debug');
-var filesize = require('gulp-filesize');
+var browserify = require('browserify');
+var notify = require("gulp-notify");
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var argv = require('yargs').argv;
+var browserSync = require('browser-sync').create();
 
 // theme assets folder
-var theme = 'src/themes/base/app/assets';
+var assets = 'src/themes/base/app/assets';
 
 /**
  *----------------------
  * Assets to combine on global file
  */
 var CSS_FILES = [
-	//theme + '/css/bootstrap.css'
+	//assets + '/css/bootstrap.css'
 ];
 
 var JS_FILES = [
-	//theme + '/js/bootstrap.min.js'
+	//assets + '/js/bootstrap.min.js'
 ];
 
 
-/**
- * ------------------------------------------
- * Assets tasks
- */
-gulp.task('assets', ['scripts', 'styles', 'sizes']);
 
 /**
  * -------------------------------------------
@@ -43,10 +45,10 @@ gulp.task('styles', function () {
 			cascade: false
 		}))
 		.pipe(concat('_global.css'))
-		.pipe(gulp.dest(theme + '/css'))
+		.pipe(gulp.dest(assets + '/css'))
 		.pipe(rename('_global.min.css'))
 		.pipe(minifyCSS({keepBreaks: false}))
-		.pipe(gulp.dest(theme + '/css'));
+		.pipe(gulp.dest(assets + '/css'));
 });
 
 /**
@@ -54,20 +56,68 @@ gulp.task('styles', function () {
  * Scripts goes here
  */
 gulp.task('lint', function () {
-	return gulp.src(theme + '/js/**/*.js')
+	return gulp.src(assets + '/js/**/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
+
 gulp.task('scripts', function () {
 	gulp.src(JS_FILES)
 		.pipe(concat('_global.js'))
 		.pipe(stripDebug())
-		.pipe(gulp.dest(theme + '/js'))
+		.pipe(gulp.dest(assets + '/js'))
 		.pipe(rename('_global.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest(theme + '/js'));
+		.pipe(gulp.dest(assets + '/js'));
 });
 
+
+/**
+ * ----------------------------------------------
+ * Example of scripts loaded with browserify
+ * run> gulp app-js -w
+ */
+gulp.task('app-js', ['browser-sync'], function () {
+
+	var nameIn = 'apps/app.js',
+		nameOut = '_app.js',
+		b = browserify(assets + '/js/' + nameIn);
+
+	function bundle() {
+		return b.bundle()
+			.pipe(source(nameOut))
+			.pipe(buffer())
+			.pipe(uglify())
+			.pipe(gulp.dest(assets + '/js'));
+	}
+
+	if (argv.w || argv.watch) {
+
+		b = watchify(b);
+		// Rebundle on update
+		b.on('update', bundle);
+		b.on('update', browserSync.reload);
+	}
+
+	return bundle();
+
+});
+
+
+/**
+ * ----------------------------------------
+ * Browser sync
+ * ----------------------------------------
+ * Add ['browser-sync'] as dependency task
+ * to reload the browser.
+ * @see package.json to configure local url
+ */
+gulp.task('browser-sync', function () {
+	browserSync.init({
+		proxy: pkg.localUrl,
+		port: 80
+	});
+});
 
 /**
  * ----------------------------------------
@@ -75,29 +125,24 @@ gulp.task('scripts', function () {
  */
 gulp.task('watch', function () {
 	gulp.watch([
-			theme + '/js/**/*.js',
-			'!' + theme + '/js/_global*.js'
+			assets + '/js/**/*.js',
+			'!' + assets + '/js/_*.js'
 		],
 		['lint', 'scripts']);
 	gulp.watch([
-			theme + '/css/*.css',
-			'!' + theme + '/css/_global*.css'
+			assets + '/css/*.css',
+			'!' + assets + '/css/_*.css'
 		],
 		['styles']);
 });
 
+
+
 /**
- * -------------------------------------------
- * Show the final size of minified versions
+ * ------------------------------------------
+ * Assets tasks
  */
-gulp.task('sizes', function(){
-
-	gulp.src(theme + '/css/_global.min.css').pipe(filesize());
-	gulp.src(theme + '/js/_global.min.js').pipe(filesize());
-
-});
+gulp.task('assets', ['scripts', 'styles']);
 
 
-
-
-gulp.task('default', ['lint', 'assets', 'watch']);
+gulp.task('default', ['assets', 'watch']);
