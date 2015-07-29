0831448ae7404aac94b1ff970218b66e6c0d4397
Bruno Barros <brunodanca@gmail.com>
2015-07-28 21:55:26 -0300

    added env.example

---
+++ .env.example.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+
+	// if the .env.local.php is present the environment will be set to 'local'
+	// by default, but you can set a personal environment configuration for some
+	// part of the application
+	'APP_ENV'     => 'local',
+
+	'APP_THEME'   => 'your-theme-folder',
+
+    'DB_HOST'     => 'localhost',
+    'DB_NAME'     => 'weloquent',
+    'DB_USER'     => 'root',
+    'DB_PASSWORD' => 'secret',
+	'DB_PREFIX'   => 'wp_',
+	'WP_HOME'     => 'http://your-domain-here',
+	'WP_SITEURL'  => 'http://your-domain-here/cms',
+
+);
